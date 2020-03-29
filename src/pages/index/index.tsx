275cb7221dd27e7fc7a5f1a40a9540607bcbb44b
谢建 <xiejian@laoyuegou.com>
2020-03-30 00:58:50 +0800

    feat: 初步完成封面

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -1,12 +1,18 @@
 import Taro, { Component, Config } from '@tarojs/taro'
-import { View, Text } from '@tarojs/components'
+import { View, Text, Swiper, SwiperItem, Audio } from '@tarojs/components'
+import 'animate.css'
 import './index.scss'
 
 export default class Index extends Component {
 
   componentWillMount () { }
 
-  componentDidMount () { }
+  componentDidMount () {
+    const innerAudioContext = Taro.createInnerAudioContext()
+    innerAudioContext.src = 'https://master-test-imgx.lygou.cc/cai/Beautiful%20In%20White.mp3'
+    innerAudioContext.loop = true;
+    innerAudioContext.play()
+  }
 
   componentWillUnmount () { }
 
@@ -22,13 +28,49 @@ export default class Index extends Component {
    * 提示和声明 navigationBarTextStyle: 'black' | 'white' 类型冲突, 需要显示声明类型
    */
   config: Config = {
-    navigationBarTitleText: '首页'
+    navigationBarTitleText: '我们的婚礼'
   }
 
   render () {
     return (
       <View className='index'>
-        <Text>Hello world!</Text>
+          {/* <Audio
+            className='audio'
+            src='https://master-test-imgx.lygou.cc/cai/Beautiful%20In%20White.mp3'
+            controls
+            loop={false}
+            autoplay
+            muted={false}
+          /> */}
+          {/* <Text className='girl'>李婕</Text>
+          <Text className='boy'>谢建</Text> */}
+          <Swiper
+            className='swiper'
+            indicatorDots={false}
+            vertical
+            circular
+            autoplay={false}
+          >
+        <SwiperItem>
+          <View className='home'>
+            <View className='image-1-wrap'>
+              <View className='image-1 animated fadeInDown'></View>
+            </View>
+            <View className='title-wrap'>
+              <View className='title animated fadeInDown animated-delay-2'></View>
+            </View>
+            <View className='sign-wrap'>
+              <View className='sign animated rollIn animated-delay-4'></View>
+            </View>
+          </View>
+        </SwiperItem>
+        <SwiperItem>
+          <View className='demo-text-2'>2</View>
+        </SwiperItem>
+        <SwiperItem>
+          <View className='demo-text-3'>3</View>
+        </SwiperItem>
+      </Swiper>
       </View>
     )
   }
